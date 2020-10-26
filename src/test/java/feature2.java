1a03f50415129c035bf5dcddce4c5f6e2a1b3f11
Murat Furuncu <murat.furuncu@gmail.com>
2020-10-26 13:20:59 +0100

    awesome feture2 ad more working

---
+++ src/test/java/feature2.java
@@ -0,0 +1,8 @@
+public class feature2 {
+
+    public static void main(String[] args) {
+        System.out.println("feuture2");
+        System.out.println("working..");
+        System.out.println("and working...");
+    }
+}
